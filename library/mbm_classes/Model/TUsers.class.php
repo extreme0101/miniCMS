d6a310bf79bdc71b72a11fe724f8a6081402dec6
Batmunkh Moltov <m.batmunkh@yahoo.com>
2013-01-13 03:12:25 +0800

    Model_TUsers.class.php updated

--- library/mbm_classes/Model/TUsers.class.php
+++ library/mbm_classes/Model/TUsers.class.php
@@ -9,6 +9,8 @@
  */
 
 /**
+ * 
+ * @Entity(repositoryClass="TUsers")
  * Description here
  *
  * @package    miniCMS
@@ -16,10 +18,51 @@
  * @author     BATMUNKH Moltov <contact@batmunkh.com>
  * @version    SVN: $Id 
  */
+//use Doctrine\ORM\EntityManager;
+
 class Model_TUsers extends TUsers {
 
-    public function Model_TUsers() {
+    public function __construct() {
         
+    }
+
+    static function getUserByUsername($username = '') {
+
+        $db = Config::get('DBCR');
+        
+        $qb = $db->createQueryBuilder();
+
+        $qb->add('select', 'u')
+                ->add('from', 'TUsers u')
+                ->add('where', 'u.username = ?1');
+
+        $qb->setParameters(array(1 => $username));
+
+        $q = $qb->getQuery();
+
+        $user = $q->getArrayResult();
+
+        return $user;
+    }
+
+    static function getUserById($user_id = 0) {
+
+        $query = Config::get('DBR')->createQuery("SELECT u FROM TUsers u");
+        $users = $query->getResult();
+
+        print_r($query->getResult());
+        die();
+        $em = Config::get('DBCR')->getRepository('TUsers');
+        $query = $em->createQueryBuilder('SELECT p FROM TUsers p')
+                ->setParameter('id', 2);
+
+        $user = $query->getResult();
+        return $user;
+    }
+    
+    public function getDBCR(){
+        
+        return parent::DBCR;
     }
 
 }
