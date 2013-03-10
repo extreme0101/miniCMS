78cc14e8e7030469523f912aaffc0681121fc1d8
Batmunkh Moltov <m.batmunkh@yahoo.com>
2013-03-10 21:52:36 +0800

    update

--- public_html/js/lib/TreeView.js
+++ public_html/js/lib/TreeView.js
@@ -1,4 +1,64 @@
-<script type="text/javascript">
-    alert("gadna ni baina oo!!!");
-    
-</script>
+jQuery(function()
+{
+    loadData(0);
+});
+
+function loadData(parentId)
+{
+    //jQuery('#browser').hide();
+    jQuery.ajax({
+        url : '/m/Category/select.ajax',
+        type : 'POST',
+        data : 'parentId=' + parentId,
+        dataType : 'json',
+        success : function(data) 
+        {
+            
+        },
+        complete : function(data) 
+        {
+            var clearData = jQuery.parseJSON(data.responseText);
+            prepareHtml(clearData, parentId);
+            catEvents();
+            $("#browser").treeview({
+                                            collapsed: true,
+                                            animated:"fast"
+                                        });             
+        } // end of complete 
+    });
+}
+
+function prepareHtml(categories, parentId)
+{
+    var html = '';
+    for(var i = 0; i < categories.length; i ++)
+    {
+        var aCategory = categories[i];
+        if(aCategory.id != 0)
+        {
+            if(aCategory.lft + 1 != aCategory.Rgt) // has children
+            {
+                html += '<li><span class = "folder" title="' + aCategory.id +'">' + aCategory.name + 
+                    '</span><ul title = ' + aCategory.id +'></ul></li>';
+            }
+            else
+            {
+                html += '<li><span class="file">' + aCategory.name +
+                    '</span></span></li>';
+            }
+            
+        }
+    }
+    jQuery("ul[title='" + parentId + "']").append(html);
+    return;
+}
+
+function catEvents()
+{
+    jQuery(".folder").each(function(){
+        jQuery(this).click(function(){
+        loadData(jQuery(this).attr("title"));
+    });
+    });
+    return;
+}
